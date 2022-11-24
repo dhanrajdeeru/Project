19a77291143abbdd1006402fe34495d78c6df072
ashwinachar <65274380+ashwinachar@users.noreply.github.com>
2022-11-24 12:01:39 +0530

    Create aselect.php
    Signed-off-by: ashwinachar <65274380+ashwinachar@users.noreply.github.com>

---
+++ aselect.php
@@ -0,0 +1,27 @@
+
+    if ("vulnerability scanning" == $lattack) {
+
+        '<script> window.location.href="attack2.html";</script>';
+
+    }
+    else if ("exploitation for defense evasion" == $lattack) {
+
+        '<script> window.location.href="attack2.html";</script>';
+
+    }
+    else if ("sql injection" == $lattack) {
+
+        '<script> window.location.href="sql_in.php";</script>';
+
+    }
+    else if ("brute force password cracking"==$lattack) {
+
+        '<script> window.location.href="attack2.html";</script>';
+
+    }  
+    else if ("account manipulation"==$lattack) {
+
+        '<script> window.location.href="attack2.html";</script>';
+
+    }
+?>
